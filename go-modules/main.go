aeb5b8a21d353910d9ff6025897e4411d36b9998
Mark Standen <mark@markstanden.uk>
2020-12-28 14:51:29 +0000

    dir changes

---
+++ go-modules/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"go.uber.org/zap"
+	quoteV3 "rsc.io/quote/v3"
+)
+
+func main(){
+	router := httprouter.New()
+	router.GET("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	logger, _ := zap.NewProduction()
+	logger.Info("Successfully performed http request.")
+	logger.Info(quoteV3.HelloV3())
+}
